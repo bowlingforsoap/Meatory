f328c5af8872f72fe962b5d08f0f24203bc33e36
LAPTOP-NB3SJRDT\Hell <stefan.hell@salmi.de>
2017-09-09 18:55:57 +0200

    win screen, new sprites in resources TODO: game is still playable after win. FIX!

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -9,11 +9,16 @@ public class BoardController : MonoBehaviour {
 	public GameObject bottomGuess; // Guess on the bottom
 	public int rows;
 	public int cols;
-	private Sprite[] sprites; // All sprites in resources folder
 
 	public Transform m_leftWin; // Position of where to place the left win
 	public Transform m_middleWin; // .. the middle win
 	public Transform m_rightWin; // .. the right win
+
+	public GameObject m_winView;
+	public GameObject m_cowPlaceHolder;
+
+	public static GameObject cowPlaceHolder;
+	public static GameObject winView;
 
 	public static Transform leftWin; // Position of where to place the left win
 	public static Transform middleWin; // .. the middle win
@@ -25,11 +30,15 @@ public class BoardController : MonoBehaviour {
 	private static int winCount = 0;
 	private static BoardController self;
 
+	private Sprite[] sprites; // All sprites in resources folder
+
 	void Start() {
 		self = this;
 		leftWin = m_leftWin;
 		middleWin = m_middleWin;
 		rightWin = m_rightWin;
+		winView = m_winView;
+		cowPlaceHolder = m_cowPlaceHolder;
 
 		Transform guessTransform;
 		Transform rightGuessTransform;
@@ -78,6 +87,8 @@ public class BoardController : MonoBehaviour {
 	}
 
 	public static void ActivateGuess(GameObject guess) {
+		cowPlaceHolder.SetActive (false);
+
 		if (guessCount >= 2) {
 			// Check if previous active are the same
 			if (activeGuesses [0] != null && activeGuesses [1] != null && activeGuesses [0].name != null && activeGuesses [0].name.Equals (activeGuesses [1].name)) {
@@ -94,11 +105,17 @@ public class BoardController : MonoBehaviour {
 					win.transform.position = rightWin.position;
 					break;
 				}
+				win.transform.localScale = new Vector3 (2f, 2f, 2f);
 
 				activeGuesses [0].SetActive (false);
 				activeGuesses [1].SetActive (false);
 
 				wins [winCount++] = win;
+
+				if (winCount == 3) { // If we win
+					//BoardController.self.gameObject.SetActive(false);
+					winView.SetActive(true);
+				}
 			} else {
 
 				// Flip the previous active
---
+++ Assets/WinController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinController : MonoBehaviour {
+	public GameObject winView;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("WinController: Win!");
+		winView.SetActive (false);
+		Application.LoadLevel (0);
+	}
+}
