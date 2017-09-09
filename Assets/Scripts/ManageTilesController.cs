3138dc04b3dbc4db0461f5030edd63e9b6f2b52b
LAPTOP-NB3SJRDT\Hell <stefan.hell@salmi.de>
2017-09-09 14:05:14 +0200

    flip tiles on click

---
+++ Assets/Scripts/ManageTilesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageTilesController : MonoBehaviour {
+	public GameObject front;
+	public GameObject back;
+
+	public void Manage(string name) {
+		Vector3 frontPosition = front.transform.position;
+		Vector3 backPosition = back.transform.position;
+
+		front.GetComponent<SpriteRenderer> ().enabled = !front.GetComponent<SpriteRenderer> ().enabled;
+		back.GetComponent<SpriteRenderer> ().enabled = !back.GetComponent<SpriteRenderer> ().enabled;
+
+		if (name.Equals ("front")) { // Flip by z component
+			front.transform.position = new Vector3 (frontPosition.x, frontPosition.y, 1f);
+			back.transform.position = new Vector3 (backPosition.x, backPosition.y, 0f);
+		} else {
+			back.transform.position = new Vector3 (backPosition.x, backPosition.y, 1f);
+			front.transform.position = new Vector3 (frontPosition.x, frontPosition.y, 0f);
+		}
+	}
+}
